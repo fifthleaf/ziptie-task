f6658f7c9fe62300d9ce6fb31cf03f13fbe12a3a
Dawid Jach <jach.developer@protonmail.com>
2024-09-01 11:42:29 +0200

    Add teacher response data validation model

--- src/ziptie/schema/teacher.py
+++ src/ziptie/schema/teacher.py
@@ -18,3 +18,41 @@ class TeacherRequest(BaseModel):
     classroom: int = Field(ge=1, le=20)
     subject: str = Field(max_length=30)
     principal: Optional[bool] = Field(default=None)
+
+
+class TeacherResponseData(TeacherRequest):
+    """
+    Represents the data returned in a teacher response.
+
+        Attributes:
+            id: The unique identifier of the teacher.
+            fullname: The full name of the teacher.
+            classroom: The classroom number of the teacher.
+            subject: The subject taught by the teacher.
+            principal: Whether the teacher is a principal or not.
+    """
+
+    id: int = Field(ge=1)
+
+    class Config:
+        """
+        Configuration class for Pydantic model.
+
+            Attributes:
+                from_attributes: Whether to create the object from attributes.
+        """
+
+        from_attributes = True
+
+
+class TeacherResponse(BaseModel):
+    """
+    Represents a response to a teacher-related request.
+
+        Attributes:
+            message: A message describing the outcome of the request.
+            data: The teacher data returned in the response.
+    """
+
+    message: str = Field(max_length=100)
+    data: TeacherResponseData
