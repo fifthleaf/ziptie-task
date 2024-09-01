ebf85b39f52481cb8ae91ace8b5f25f73ed162df
Dawid Jach <jach.developer@protonmail.com>
2024-09-01 12:37:32 +0200

    Add yearbook data retrieval API endpoint

--- src/ziptie/main.py
+++ src/ziptie/main.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
-from fastapi import Body, FastAPI, HTTPException
+from fastapi import Body, FastAPI, HTTPException, Path, Query
+from sqlalchemy.sql import select
 
 from . import db, schema, utils
 
@@ -58,6 +59,50 @@ async def create_student(
         return schema.StudentResponse(
             message="Student created successfully",
             data=schema.StudentResponseData.from_orm(student),
+        )
+    except HTTPException as exc:
+        raise exc
+    except Exception as exc:
+        raise HTTPException(
+            status_code=500, detail="Something went wrong"
+        ) from exc
+
+
+@app.get("/yearbook/{teacher_id}")
+async def get_yearbook(
+    teacher_id: Annotated[int, Path(ge=1)],
+    page_size: Annotated[int, Query(ge=1)] = 10,
+    page: Annotated[int, Query(ge=0)] = 0,
+) -> schema.YearbookResponse:
+    """
+    Retrieves the yearbook for a specific teacher.
+
+        Args:
+            teacher_id: The ID of the teacher.
+            page_size: The number of students to return per page.
+            page: The page number to return.
+
+        Returns:
+            Success message and the yearbook data.
+    """
+    try:
+        query = (
+            select(
+                db.Student.fullname.label("student"),
+                db.Student.major,
+                db.Teacher.fullname.label("teacher"),
+                db.Teacher.classroom,
+            )
+            .join(db.Teacher.students)
+            .where(db.Teacher.id == teacher_id)
+            .limit(page_size)
+            .offset(page)
+        )
+        with db.local_session() as session:
+            yearbook = utils.get_data(session, query)
+        return schema.YearbookResponse(
+            message=f"Class of a teacher: {teacher_id}",
+            data=yearbook,
         )
     except HTTPException as exc:
         raise exc
